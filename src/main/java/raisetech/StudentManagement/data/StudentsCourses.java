199ae58c82e21901c9cecb88a069c486246ac10a
kitapan <kitapanpan@gmail.com>
2024-08-31 21:44:20 +0900

    Merge remote-tracking branch 'origin/kadai' into kadai

---
+++ src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
@@ -0,0 +1,17 @@
+package raisetech.StudentManagement.data;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class StudentsCourses {
+
+  public String id;
+  public String studentId;
+  public String courseName;
+  public LocalDateTime courseStartAt;
+  public LocalDateTime courseEndAt;
+}
